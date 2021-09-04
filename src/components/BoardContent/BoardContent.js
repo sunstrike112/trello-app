9f7a1d377b2c634f2beb21c5d4a6f938e0d07460
phaptran <phaptran112@gmail.com>
2021-09-04 22:03:27 +0700

    inittial sample data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { isEmpty } from "lodash";
+
+import { initialData } from "actions/initialData";
+import { mapOrder } from "utilities/sorts";
 
 import Column from "components/Column/Column";
 import "./BoardContent.scss";
 
 function BoardContent(props) {
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find((board) => board.id === "board-1");
+    if (boardFromDB) {
+      setBoard(boardFromDB);
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
+    }
+  }, []);
+
+  if (isEmpty(board)) {
+    return (
+      <div className="not-found" style={{ padding: "10px", color: "white" }}>
+        Board not found
+      </div>
+    );
+  }
   return (
     <div className="board-content">
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
+      {columns.map((column, index) => (
+        <Column key={index} column={column} />
+      ))}
     </div>
   );
 }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-import Task from "components/Task/Task";
+import Card from "components/Card/Card";
 import "./Column.scss";
+import { mapOrder } from "utilities/sorts";
 
 function Column(props) {
+  const { column } = props;
+  const cards = mapOrder(column.cards, column.cardOrder, "id");
+
   return (
     <div className="column">
-      <header>BrainStorm</header>
-      <ul className="task-list">
-        <Task />
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
+      <header>{column.title}</header>
+      <ul className="card-list">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
       </ul>
       <footer>Add another card</footer>
     </div>
