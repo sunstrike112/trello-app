2d064c3593706ae967c02c3a19f57718c9f9f58a
phaptran <phaptran112@gmail.com>
2021-09-05 21:34:27 +0700

    implement bootstrap, react-bootstrap, add new column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { isEmpty } from "lodash";
 import { Container, Draggable } from "react-smooth-dnd";
+import { Container as BootstrapContainer, Row, Col, Form, Button } from "react-bootstrap";
 
 import { initialData } from "actions/initialData";
 import { mapOrder } from "utilities/sorts";
@@ -12,6 +13,12 @@ import "./BoardContent.scss";
 function BoardContent(props) {
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+  const [newColumnTitle, setNewColunmTile] = useState("");
+
+  const newColumInputRef = useRef(null);
+
+  const onNewColumnTitleChange = useCallback((e) => setNewColunmTile(e.target.value), []);
 
   useEffect(() => {
     const boardFromDB = initialData.boards.find((board) => board.id === "board-1");
@@ -20,6 +27,13 @@ function BoardContent(props) {
       setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
     }
   }, []);
+
+  useEffect(() => {
+    if (newColumInputRef && newColumInputRef.current) {
+      newColumInputRef.current.focus();
+      newColumInputRef.current.select();
+    }
+  }, [openNewColumnForm]);
 
   if (isEmpty(board)) {
     return (
@@ -49,6 +63,33 @@ function BoardContent(props) {
     }
   };
 
+  const toggleOpenNewColumnForm = () => {
+    setOpenNewColumnForm(!openNewColumnForm);
+  };
+
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      newColumInputRef.current.focus();
+      return;
+    }
+    const newColumnToAdd = {
+      id: Math.random().toString(36).substr(2, 5),
+      boardId: board.id,
+      title: newColumnTitle.trim(),
+      cardOrder: [],
+      cards: [],
+    };
+    let newColumns = [...columns];
+    newColumns.push(newColumnToAdd);
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((column) => column.id);
+    newBoard.columns = newColumns;
+    setColumns(newColumns);
+    setBoard(newBoard);
+    setNewColunmTile("");
+    toggleOpenNewColumnForm();
+  };
+
   return (
     <div className="board-content">
       <Container
@@ -68,9 +109,37 @@ function BoardContent(props) {
           </Draggable>
         ))}
       </Container>
-      <div className="add-new-column">
-        <i className="fa fa-plus icon" /> Add another column
-      </div>
+      <BootstrapContainer className="trello-container">
+        {!openNewColumnForm && (
+          <Row>
+            <Col className="add-new-column" onClick={toggleOpenNewColumnForm}>
+              <i className="fa fa-plus icon" /> Add another column
+            </Col>
+          </Row>
+        )}
+        {openNewColumnForm && (
+          <Row>
+            <Col className="enter-new-column">
+              <Form.Control
+                size="sm"
+                type="text"
+                placeholder="Enter column title ..."
+                className="input-enter-new-column"
+                ref={newColumInputRef}
+                value={newColumnTitle}
+                onChange={onNewColumnTitleChange}
+                onKeyDown={(event) => event.key === "Enter" && addNewColumn()}
+              />
+              <Button variant="success" size="sm" onClick={addNewColumn}>
+                Add column
+              </Button>
+              <span className="cancel-new-column" onClick={toggleOpenNewColumnForm}>
+                <i className="fa fa-trash icon" />
+              </span>
+            </Col>
+          </Row>
+        )}
+      </BootstrapContainer>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-import Column from "components/Column/Column";
 import "./Card.scss";
 
 function Card(props) {
   const { card } = props;
 
+  const onMouseDown = (e) => {
+    if (e.cancelable) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  };
+
   return (
     <div className="card-item">
-      {card.cover && <img className="card-cover" src={card.cover} draggable="false" />}
+      {card.cover && (
+        <img className="card-cover" src={card.cover} onMouseDown={onMouseDown} />
+      )}
       {card.title}
     </div>
   );
