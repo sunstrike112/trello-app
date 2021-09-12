548e12cc3a1dbe977a2f65821e987c72f58b6d76
phaptran <phaptran112@gmail.com>
2021-09-12 07:29:16 +0700

    fix add card

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -110,7 +110,7 @@ function Column(props) {
             />
 
             <Dropdown.Menu>
-              <Dropdown.Item>Add card...</Dropdown.Item>
+              <Dropdown.Item onClick={toggleOpenNewCardForm}>Add card...</Dropdown.Item>
               <Dropdown.Item onClick={toggleShowConfirmModal}>
                 Remove column...
               </Dropdown.Item>
