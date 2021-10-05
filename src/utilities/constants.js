a65e0570d40d84ac94b45c26aa8ca4ea5a8eb9fb
phaptran <phaptran112@gmail.com>
2021-10-06 02:55:49 +0700

    Deploy trelloapp to Firebase

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,5 @@
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
+export const API_ROOT = 'https://trello-app-phaptdq.herokuapp.com'
 
 export const MODAL_ACTION_CLOSE = "MODAL_ACTION_CLOSE";
 export const MODAL_ACTION_CONFIRM = "MODAL_ACTION_CONFIRM";
